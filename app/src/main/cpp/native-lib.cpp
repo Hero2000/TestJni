91da9e35f13ae225970ab07125f2e03b240dedb8
Hero <yinyibo1985@163.com>
2020-09-21 11:23:58 +0800

    testJni first commit

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,187 @@
+#include <jni.h>
+#include <string>
+#include <android/log.h>
+
+// __VA_ARGS__ 代表 ...的可变参数
+#define TAG "native-lib"
+
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, TAG,  __VA_ARGS__);
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, TAG,  __VA_ARGS__);
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, TAG,  __VA_ARGS__);
+
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_example_testjni_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    LOGD("12334 %d",12);
+    return env->NewStringUTF(hello.c_str());
+}
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_example_testjni_MainActivity_stringFromJNI2(JNIEnv *env, jobject thiz) {
+    // TODO: implement stringFromJNI2()
+    std::string hello = "stringFromJNI2";
+    LOGD("12334 %d",12);
+    return env->NewStringUTF(hello.c_str());
+}extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_testjni_MainActivity_test1(JNIEnv *env, jobject instance,
+                                              jboolean jboolean1,
+                                              jbyte jbyte1,
+                                              jchar jchar1,
+                                              jshort jshort1,
+                                              jlong jlong1,
+                                              jfloat jfloat1,
+                                              jdouble jdouble1,
+                                              jstring name_,
+                                              jint age,
+                                              jintArray i_,
+                                              jobjectArray strs,
+                                              jobject person,
+                                              jbooleanArray bArray_) {
+
+    //1. 接收 Java 传递过来的 boolean 值
+    unsigned char b_boolean = jboolean1;
+    LOGD("boolean-> %d", b_boolean);
+
+    //2. 接收 Java 传递过来的 boolean 值
+    char c_byte = jbyte1;
+    LOGD("jbyte-> %d", c_byte);
+
+    //3. 接收 Java 传递过来的 char 值
+    unsigned short c_char = jchar1;
+    LOGD("char-> %d", c_char);
+
+
+    //4. 接收 Java 传递过来的 short 值
+    short s_short = jshort1;
+    LOGD("short-> %d", s_short);
+
+
+    //5. 接收 Java 传递过来的 long 值
+    long l_long = jlong1;
+    LOGD("long-> %d", l_long);
+
+    //6. 接收 Java 传递过来的 float 值
+    float f_float = jfloat1;
+    LOGD("float-> %f", f_float);
+
+
+    //7. 接收 Java 传递过来的 double 值
+    double d_double = jdouble1;
+    LOGD("double-> %f", d_double);
+
+
+    //8. 接收 Java 传递过来的 String 值
+
+    jsize  length = env->GetStringUTFLength(name_);
+    LOGD("length-> %d", length);
+
+
+    const char *name_string = env->GetStringUTFChars(name_, 0);
+    LOGD("string-> %s", name_string);
+
+    //9. 接收 Java 传递过来的 int 值
+    int age_java = age;
+    LOGD("int:%d", age_java);
+
+    //10. 打印 Java 传递过来的 int []
+    jint *intArray = env->GetIntArrayElements(i_, NULL);
+    //拿到数组长度
+    jsize intArraySize = env->GetArrayLength(i_);
+    for (int i = 0; i < intArraySize; ++i) {
+        LOGD("intArray->%d：", intArray[i]);
+    }
+    //释放数组
+    env->ReleaseIntArrayElements(i_, intArray, 0);
+
+
+    //11. 打印 Java 传递过来的 String[]
+    jsize stringArrayLength = env->GetArrayLength(strs);
+    for (int i = 0; i < stringArrayLength; ++i) {
+        jobject jobject1 = env->GetObjectArrayElement(strs, i);
+        //强转 JNI String
+        jstring stringArrayData = static_cast<jstring >(jobject1);
+
+        //转 C  String
+        const char *itemStr = env->GetStringUTFChars(stringArrayData, NULL);
+        LOGD("String[%d]: %s", i, itemStr);
+        //回收 String[]
+        env->ReleaseStringUTFChars(stringArrayData, itemStr);
+        env->DeleteLocalRef(jobject1); // 回收
+
+    }
+
+    //12. 打印 Java 传递过来的 Object 对象
+    //12.1 获取字节码
+    const char *person_class_str = "com/example/testjni/Person";
+    //12.2 转 jni jclass
+    jclass person_class = env->FindClass(person_class_str);
+    //12.3 拿到方法签名 javap -a
+    const char *sig = "()Ljava/lang/String;";
+    jmethodID jmethodID1 = env->GetMethodID(person_class, "getName", sig);
+
+    jobject obj_string = env->CallObjectMethod(person, jmethodID1);
+    jstring perStr = static_cast<jstring >(obj_string);
+    const char *itemStr2 = env->GetStringUTFChars(perStr, NULL);
+    LOGD("Person: %s", itemStr2);
+
+    env->ReleaseStringUTFChars(perStr, itemStr2);
+    env->DeleteLocalRef(person_class); // 回收
+    env->DeleteLocalRef(person); // 回收
+
+    //13. 打印 Java 传递过来的 booleanArray
+    jsize booArrayLength = env->GetArrayLength(bArray_);
+    jboolean *bArray = env->GetBooleanArrayElements(bArray_, NULL);
+    for (int i = 0; i < booArrayLength; ++i) {
+        bool b =  bArray[i];
+        jboolean b2 =  bArray[i];
+        LOGD("boolean:%d",b)
+        LOGD("jboolean:%d",b2)
+    }
+    //回收
+    env->ReleaseBooleanArrayElements(bArray_, bArray, 0);
+
+
+}extern "C"
+JNIEXPORT jobject JNICALL
+Java_com_example_testjni_MainActivity_getPerson(JNIEnv *env, jobject thiz) {
+    // TODO: implement getPerson()
+    //1. 拿到 Java 类的全路径
+    const char *person_java = "com/example/testjni/Person";
+    const char *method = "<init>"; // Java构造方法的标识
+
+    //2. 找到需要处理的 Java 对象 class
+    jclass j_person_class = env->FindClass(person_java);
+
+    //3. 拿到空参构造方法
+    jmethodID person_constructor = env->GetMethodID(j_person_class, method, "()V");
+
+    //4. 创建对象
+    jobject person_obj = env->NewObject(j_person_class, person_constructor);
+
+    //5. 拿到 setName 方法的签名，并拿到对应的 setName 方法
+    const char *nameSig = "(Ljava/lang/String;)V";
+    jmethodID nameMethodId = env->GetMethodID(j_person_class, "setName", nameSig);
+
+    //6. 拿到 setAge 方法的签名，并拿到 setAge 方法
+    const char *ageSig = "(I)V";
+    jmethodID ageMethodId = env->GetMethodID(j_person_class, "setAge", ageSig);
+
+    //7. 正在调用 Java 对象函数
+    const char *name = "Hero";
+    jstring newStringName = env->NewStringUTF(name);
+    env->CallVoidMethod(person_obj, nameMethodId, newStringName);
+    env->CallVoidMethod(person_obj, ageMethodId, 28);
+
+    const char *sig = "()Ljava/lang/String;";
+    jmethodID jtoString = env->GetMethodID(j_person_class, "toString", sig);
+    jobject obj_string = env->CallObjectMethod(person_obj, jtoString);
+    jstring perStr = static_cast<jstring >(obj_string);
+    const char *itemStr2 = env->GetStringUTFChars(perStr, NULL);
+    LOGD("Person: %s", itemStr2);
+    return person_obj;
+
+}
---
+++ app/src/main/java/com/example/testjni/Person.java
@@ -0,0 +1,44 @@
+package com.example.testjni;
+
+public class Person {
+
+    private  String name;
+    private  int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Person() {
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+}
